a7054813923210299cddf0b1d24596fc828e7df5
aristotlemanolakos <ari@stotle.me>
2017-02-19 11:52:54 -0600

    First commit

---
+++ keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+#keys.py
+#visit http://dev.twitter.com to create an application and get your keys
+keys = dict(
+	consumer_key =          'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+	consumer_secret =       'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+	access_token =          'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+	access_token_secret =   'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+)
