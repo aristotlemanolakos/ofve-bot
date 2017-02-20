f4bb065468a33d586e3ba4599eda23a2cf5d8fd5
aristotlemanolakos <ari@stotle.me>
2017-02-19 20:49:44 -0600

    Updated search. Need to rewrite for stream

--- search.py
+++ search.py
@@ -9,18 +9,22 @@ CONSUMER_SECRET = keys['consumer_secret']
 ACCESS_TOKEN = keys['access_token']
 ACCESS_TOKEN_SECRET = keys['access_token_secret']
 
-
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
 api = tweepy.API(auth)
 
-twts = api.search(q='"could of" OR "would of" OR "should of" -filter:retweets')
+twts = api.search(q='"could of" OR "would of" OR "should of" -filter:retweets', count=100, lang="en", result_type="recent")
 
 #list of specific strings we want to check for in Tweets
-t = ['could of']
+#t = ['could of']
+
+CYELLOW = '\33[33m'
+CEND = '\033[0m'
+CBOLD = '\33[1m'
 
 for s in twts:
-    print s.text + "\n" # Let's see what's in s.text
+	print(CYELLOW + CBOLD + '<' + s.user.screen_name + '>:' + CEND + ' ' + s.text + '\n')
+    #print "<" + s.user.screen_name + ">: " + s.text + "\n" # Let's see what's in s.text
     #for i in t:
         #if i == s.text:
             #sn = s.user.screen_name
